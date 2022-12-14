73ca45128432cdc256001c9ddd06654a9e1498a1
LuisMedei <Luisgreen363@gmail.com>
2022-12-13 20:11:58 -0500

    terraform code

---
+++ awsec2.tf
@@ -0,0 +1,6 @@
+
+
+resource "aws_instance" "terraform_demo" {
+    ami = "ami-0b0dcb5067f052a63"
+    instance_type = "t2.micro"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+region = "us-east-1"
+ami = "ami-0b0dcb5067f052a63"
+instance-type = "t2.micro"
